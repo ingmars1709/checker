23bba7fec0574cd0648cd77182788ee7360eaaf1
Ingmar <ingmars@infosupport.com>
2019-10-18 10:19:22 +0200

    Refactor

--- src/main/java/com/infosupport/checker/examples/EncryptExamples.java
+++ src/main/java/com/infosupport/checker/examples/EncryptExamples.java
@@ -7,25 +7,22 @@ import java.util.Base64;
 
 public class EncryptExamples {
 
-    public static final Charset UTF_8_CHARSET = Charset.forName("UTF-8");
+    public static final Charset UTF8 = Charset.forName("UTF-8");
 
     private static final String CONFIDENTIAL_PLAIN_DATA = "confidential data";
-    private static final byte[] CONFIDENTIAL_PLAIN_DATA_BYTES = CONFIDENTIAL_PLAIN_DATA.getBytes(UTF_8_CHARSET);
+    private static final byte[] CONFIDENTIAL_PLAIN_DATA_BYTES = CONFIDENTIAL_PLAIN_DATA.getBytes(UTF8);
+    private static final byte KEY = 3;
 
     public static void main(String[] args) {
         EncryptExamples example = new EncryptExamples();
 
         example.sendEncryptedData();
+
         example.sendUnencryptedData();
-        example.decryptData();
-    }
 
-    private void decryptData() {
-        byte[] plaintext = decrypt(encrypt(CONFIDENTIAL_PLAIN_DATA));
+        example.decryptEncryptedData();
 
-        System.out.println("decrypted plain text = " + new String(plaintext, UTF_8_CHARSET));
-
-        byte[] wrong = decrypt(CONFIDENTIAL_PLAIN_DATA_BYTES);
+        example.decryptDecryptedData();
     }
 
     private void sendEncryptedData() {
@@ -41,21 +38,30 @@ public class EncryptExamples {
         System.out.println("sent: " + Base64.getEncoder().encodeToString(ciphertext));
     }
 
+    private void decryptEncryptedData() {
+        byte[] plaintext = decrypt(encrypt(CONFIDENTIAL_PLAIN_DATA));
+
+        System.out.println("decrypted plain text = " + new String(plaintext, UTF8));
+    }
+
+    private void decryptDecryptedData() {
+        byte[] wrong = decrypt(CONFIDENTIAL_PLAIN_DATA_BYTES);
+    }
+
     @SuppressWarnings("encrypted") // type system does not inspect method body to see if actual encryption happens
     private @Encrypted byte[] encrypt(final String plaintext) {
-        byte[] bytesOfPlainText = plaintext.getBytes(UTF_8_CHARSET);
-        return xorIt(bytesOfPlainText);
+        return xorIt(plaintext.getBytes(UTF8), KEY);
     }
 
     private byte[] decrypt(final @Encrypted byte[] ciphertext) {
-        return xorIt(ciphertext);
+        return xorIt(ciphertext, KEY);
     }
 
-    private byte[] xorIt(final byte[] inputBytes) {
+    private byte[] xorIt(final byte[] inputBytes, final byte key) {
         byte[] outputBytes = new byte[inputBytes.length];
 
         for (int i = 0; i < inputBytes.length; i++) {
-            outputBytes[i] = (byte) (inputBytes[i] ^ (byte) 3);
+            outputBytes[i] = (byte) (inputBytes[i] ^ key);
         }
         return outputBytes;
     }
