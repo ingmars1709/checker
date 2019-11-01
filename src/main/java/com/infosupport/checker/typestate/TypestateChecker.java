78c6434706ef23d06ea3fe361247f20422f3c81a
Ingmar <ingmars@infosupport.com>
2019-11-01 11:59:25 +0100

    Rename

--- src/main/java/com/infosupport/checker/typestate/TypestateChecker.java
+++ src/main/java/com/infosupport/checker/typestate/TypestateChecker.java
@@ -2,6 +2,6 @@ package com.infosupport.checker.typestate;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
-public class FooChecker extends BaseTypeChecker {
+public class TypestateChecker extends BaseTypeChecker {
 
 }
