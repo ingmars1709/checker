bb972481c62d529f99117f0f3b4ecd68ad3a3577
Ingmar <ingmars@infosupport.com>
2020-01-17 11:16:00 +0100

    Removed default

--- src/main/java/com/infosupport/checker/qual/Encrypted.java
+++ src/main/java/com/infosupport/checker/qual/Encrypted.java
@@ -1,13 +1,10 @@
 package com.infosupport.checker.qual;
 
-import org.checkerframework.framework.qual.DefaultFor;
 import org.checkerframework.framework.qual.SubtypeOf;
-import org.checkerframework.framework.qual.TypeUseLocation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @SubtypeOf(PossiblyUnencrypted.class)
-@DefaultFor({TypeUseLocation.LOWER_BOUND})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Encrypted {}
